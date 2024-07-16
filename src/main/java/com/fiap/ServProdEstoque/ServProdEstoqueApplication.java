651323316fbc47a87af1432bb58368bf5484e498
Leandro Conceição <leandro.vs2010@hotmail.com>
2024-07-15 21:32:27 -0300

    Estrutura do projeto

---
+++ src/main/java/com/fiap/ServProdEstoque/ServProdEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ServProdEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServProdEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServProdEstoqueApplication.class, args);
+	}
+
+}
